8a8316249fad290b88ff11cf01d00830dafdf9fb
Adam Jetmalani <adam@getlittlebird.com>
2015-05-23 13:32:11 -0700

    use hsl instead of rgb

--- src-cljs/home/page/sorter.cljs
+++ src-cljs/home/page/sorter.cljs
@@ -1,18 +1,10 @@
 (ns home.page.sorter)
 
-(defn random-color
-  []
-  (subs 
-   (.toString
-    (+ 16r1000000 (rand-int 16rFFFFFF))
-    16)
-   1))
-
 (defn generate-item
   [rank-init]
   [rank-init 
    {:rank rank-init
-    :color (str "#" (random-color))}])
+    :hue (rand-int 360)}])
 
 (defn generate-items
   [n]
@@ -63,7 +55,7 @@
    update-in [:page :items]
    (fn 
      [item-map]
-     (let [sorted (sort-by #(-> % (get 1) :color) item-map)]
+     (let [sorted (sort-by #(-> % (get 1) :hue) item-map)]
        (into
         {}
         (map-indexed
@@ -78,14 +70,13 @@
     (str "translate3d("h-offset"%, "v-offset"%, 0)")))
 
 (defn list-item
-  [num-columns id item]
+  [num-columns hue-shift id {:keys [hue rank]}]
   [:div.item 
    {:style 
-    {:background-color (:color item)
+    {:background-color (str "hsl(" (+ hue hue-shift) ", 100%, 50%)")
      :z-index (- id)
-     :transform (grid-place num-columns (:rank item))}
-    :id id}
-   ])
+     :transform (grid-place num-columns rank)}
+    :id id}])
 
 (defn controls
   [state]
@@ -93,19 +84,18 @@
         num-items (count (get-in @state [:page :items]))]
     [:div#sortable-controls
      [:div.row
-      [:input {:type "number"
-               :value num-items
-               :on-change #(swap! state assoc-in [:page :items] (-> % .-target .-value js/parseInt generate-items))}]
+      [:input 
+       {:type "number"
+        :value num-items
+        :on-change #(swap! state assoc-in [:page :items] (-> % .-target .-value js/parseInt generate-items))}]
       [:span "# of items: " num-items]]
      [:div.row 
-      [:input {:type "range"
-               :min 1
-               :max 30
-               :value num-columns
-               :on-change #(swap! 
-                            state 
-                            assoc-in [:page :num-columns] 
-                            (-> % .-target .-value js/parseInt))}]
+      [:input 
+       {:type "range"
+        :min 1
+        :max 30
+        :value num-columns
+        :on-change #(swap! state assoc-in [:page :num-columns] (-> % .-target .-value js/parseInt))}]
       "# of columns: " num-columns]
      [:div.row
       [:button {:on-click #(shuffle-items! state)} "Shuffle"]
@@ -117,13 +107,15 @@
   (swap! 
    state 
    assoc :page 
-   {:items (generate-items 1000)
-    :num-columns 25})
+   {:items (generate-items 100)
+    :num-columns 10
+    :hue-shift 0})
+  (.setInterval js/window #(swap! state update-in [:page :hue-shift] + 36) 5000)
   (fn
     [state]
-    (let [{:keys [num-columns items]} (:page @state)]
+    (let [{{:keys [num-columns items hue-shift]} :page} @state]
       [:div
        [controls state]
        [:div#sortable-list
         (for [[id item] items]
-          ^{:key id} [list-item num-columns id item])]])))
+          ^{:key id} [list-item num-columns hue-shift id item])]])))
