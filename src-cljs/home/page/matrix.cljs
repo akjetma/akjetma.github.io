ad19c95c46fc1e6ec87ee0a2c09d17f9153728ec
Adam Jetmalani <adam@getlittlebird.com>
2015-05-26 22:01:42 -0700

    even cooler

--- src-cljs/home/page/matrix.cljs
+++ src-cljs/home/page/matrix.cljs
@@ -3,10 +3,10 @@
 
 (defn index-controller
   [state i j]
-  [:input 
+  [:input.mat-slider 
    {:type "range"
-    :min 0
-    :max 1
+    :min -2
+    :max 2
     :step 0.001
     :value (get-in @state [:page :matrix i j])
     :on-change #(swap! state assoc-in [:page :matrix i j] 
@@ -14,23 +14,27 @@
 
 (defn control-mat
   [state]
-  [:div
+  [:div.mat-controls
    (for [i (range 2)] 
      ^{:key i} 
-     [:div 
+     [:div.row
       (for [j (range 3)]
         ^{:key j} 
         [index-controller state i j])])])
 
+(defn render-mat
+  [a b tx c d ty & [child]]
+  [:div.mat 
+   {:style 
+    {:transform (str "matrix(" (->> [a c b d (* 10 tx) (* 10 ty)] (interpose ",") (apply str)) ")")}}
+   child])
+
 (defn display-mat
   [state]
-  (let [{{[[a b tx] [c d ty]] :matrix} :page} @state]
-    [:div 
-     {:style 
-      {:width "200px"
-       :height "200px"
-       :background-color "#000"
-       :transform (str "matrix(" (->> [a c b d tx ty] (interpose ",") (apply str)) ")")}}]))
+  (let [{{[[a b tx] [c d ty]] :matrix} :page} @state
+        renderable (partial vector render-mat a b tx c d ty)]
+    [:div.mat-container
+     (reduce (fn [nest render-fn] (render-fn nest)) (renderable) (repeat 25 renderable))]))
 
 (defn page
   [state]
@@ -40,6 +44,7 @@
           [0 1 0]])
   (fn 
     [state]
-    [:div
+    [:div#matrix-page
+     [:p.help "Use arrow keys for fine adjustments"]
      [control-mat state]
      [display-mat state]]))
