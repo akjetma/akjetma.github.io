a0524a3870804fe7a0832b991fdd0692596f6a60
Adam Jetmalani <adam@getlittlebird.com>
2015-05-31 04:14:57 -0700

    basic 3d cube

--- src-cljs/home/page/cube.cljs
+++ src-cljs/home/page/cube.cljs
@@ -1,5 +1,31 @@
 (ns home.page.cube)
 
+(defn get-active-face
+  [ind]
+  (str 
+   (last 
+    (take 
+     (inc ind) 
+     (cycle ["front" "back" "right" "left" "top" "bottom"])))
+   "-active"))
+
+(defn cube
+  [state]
+  (let [face-index (get-in @state [:page :face-index])]
+    [:div.cube-container
+     [:div {:class (str "cube " (get-active-face face-index))
+            :on-click #(swap! state update-in [:page :face-index] inc)}
+      [:div.face.front]
+      [:div.face.back]
+      [:div.face.right]
+      [:div.face.left]
+      [:div.face.top]
+      [:div.face.bottom]]]))
+
 (defn page
   [state]
-  [:div])
+  (swap! state assoc-in [:page :face-index] 0)
+  (fn 
+    [state] 
+    [:div#cube-page
+     [cube state]]))
