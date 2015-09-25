cd83cb4803a29f3bd95042a3f1a817969f4ee966
Adam Jetmalani <adam@getlittlebird.com>
2015-09-25 00:44:34 -0700

    split up canvases

--- src-cljs/home/page/camera.cljs
+++ src-cljs/home/page/camera.cljs
@@ -11,45 +11,56 @@
     true))
 
 (defn render-loop
-  [context video]
-  (.drawImage context video 0 0)
-  (.setTimeout js/window #(render-loop context video) framerate))
+  [contexts video]
+  (let [num (count contexts)
+        width (.-videoWidth video)
+        height (quot (.-videoHeight video) num)]
+    (doall
+     (map-indexed
+      (fn [n context]        
+        (.drawImage context video 0 (* n height) width height 0 0 width height))
+      contexts)))
+  (.setTimeout js/window #(render-loop contexts video) framerate))
 
 (defn start-loop
-  [context video object-url]
+  [contexts video object-url]
   (set! (.-src video) object-url)
-  (render-loop context video))
+  (.setTimeout js/window #(render-loop contexts video) 1000))
 
 (defn got-media
-  [state context video stream]
+  [state contexts video stream]
   (let [object-url (-> js/window .-URL (.createObjectURL stream))]
     (swap!
      state assoc
      :camera-stream stream
      :camera-object-url object-url)
-    (start-loop context video object-url)))
+    (start-loop contexts video object-url)))
 
 
 
 
 
 (defn output-render
-  [state]
-  [:canvas#output (:camera-dimensions @state)])
+  [state total n]
+  (let [id (str "output-" n)
+        dimensions (:camera-dimensions @state)
+        width (:width dimensions)
+        height (quot (:height dimensions) total)]
+    [:canvas.output
+     {:id id :width width :height height}]))
 
 (defn output-did-mount
-  [state]
-  (let [canvas (.querySelector js/document "#output")
+  [state n]
+  (.log js/console "output-did-mount")
+  (let [canvas (.querySelector js/document (str "#output-" n))
         context (.getContext canvas "2d")]
-    (swap! state assoc
-           :camera-canvas canvas
-           :camera-context context)))
+    (swap! state update-in [:camera-contexts] (fnil conj []) context)))
 
 (defn output
-  [state]
+  [state total n]
   (reagent/create-class
    {:reagent-render output-render
-    :component-did-mount #(output-did-mount state)}))
+    :component-did-mount #(output-did-mount state n)}))
 
 
 
@@ -60,13 +71,13 @@
 
 (defn input-did-mount
   [state]
+  (.log js/console "input-did-mount")
   (let [video (.querySelector js/document "#input")]
     (swap! state assoc :camera-video video)
     (.addEventListener
      video "canplay"
      #(swap! state assoc
-             :camera-dimensions {:width (.-videoWidth video) :height (.-videoHeight video) :style {:height "110vh"}}))
-    ))
+             :camera-dimensions {:width (.-videoWidth video) :height (.-videoHeight video)}))))
 
 (defn input
   [state]
@@ -79,24 +90,29 @@
 
 (defn page-render
   [state]
-  [:div#camera-page
-   [output state]
-   [input state]])
+  (let [canvases (:camera-canvas-count @state)]
+    [:div#camera-page
+     [input state]
+     (for [n (range canvases)]
+       ^{:key n}
+       [output state canvases n])]))
 
 (defn page-did-mount
   [state]
-  (let [{:keys [camera-context camera-video camera-object-url]} @state]
+  (.log js/console "page-did-mount")
+  (let [{:keys [camera-contexts camera-video camera-object-url]} @state]
     (if camera-object-url
-      (start-loop camera-context camera-video camera-object-url) 
+      (start-loop camera-contexts camera-video camera-object-url) 
       (if (set-gum?)
         (.getUserMedia
          js/navigator #js {:video true}
-         (partial got-media state camera-context camera-video)
+         (partial got-media state camera-contexts camera-video)
          #(.log js/console "error: " %))
         (swap! state assoc :camera-error :unsupported)))))
 
 (defn page
   [state]
+  (swap! state assoc :camera-canvas-count 10)
   (reagent/create-class
    {:reagent-render page-render
     :component-did-mount #(page-did-mount state)}))
