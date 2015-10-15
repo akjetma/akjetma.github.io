0cc9d02794a34471d2ddfc3be074c0dbba014e3f
Adam Jetmalani <adam@getlittlebird.com>
2015-10-14 18:51:40 -0700

    update all deps

--- project.clj
+++ project.clj
@@ -1,13 +1,17 @@
 (defproject home "0.1.0"
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                 [org.clojure/clojurescript "0.0-3269"]
-                 [reagent "0.5.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]
+                 [reagent "0.5.1"]
                  [secretary "1.2.3"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-figwheel "0.3.1"]]
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.1"]]
 
-  :clean-targets ^{:protect false} ["resources/public/js" "target"]
+  :clean-targets ^{:protect false} ["resources/public/js/app.dev.js"
+                                    "resources/public/js/app.min.js"
+                                    "resources/public/js/out"
+                                    "resources/public/css/home.css"
+                                    "target"]
 
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src-cljs"]
