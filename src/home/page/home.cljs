d51fe1e8b04cd3d5e8a9b8a10e24c01c075cf8e4
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2016-03-21 07:58:02 -0700

    TMI

--- src/home/page/home.cljs
+++ src/home/page/home.cljs
@@ -6,8 +6,6 @@
   [:div#home-page
    [:div.box
     [:ul
-     [:li "Hello! My name is Adam and sometimes I make web toys."]
-     [:li "This website also serves as an example project for a Clojure/Clojurescript web app."]
-     [:li "The tiny API is hosted on Heroku. Source code " [:a.link {:href "https://github.com/akjetma/akjetma-back"} "here"]]
-     [:li "The client is a static page hosted via Github Pages. Source code " [:a.link {:href "https://github.com/akjetma/akjetma.github.io"} "here"]]
-     [:li "For more info about me, see my resume " [:a.link {:href (util/resource state "/resume.html")} "here"]]]]])
+     [:li "Hi, this is my dumb Clojurescript web app thing."]
+     [:li "The client is a statically hosted Github Pages type thing. " [:a.link {:href "https://github.com/akjetma/akjetma.github.io"} "[src]"]]
+     [:li "There's a little API running on Heroku for the wordcloud thing. " [:a.link {:href "https://github.com/akjetma/akjetma-back"} "[src]"]]]]])
