cd78b2f3f0f9fa155af9a6fd123276bb462a9033
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2016-04-18 21:54:46 -0700

    make more brutalist

--- src/home/page/home.cljs
+++ src/home/page/home.cljs
@@ -4,8 +4,4 @@
 (defn page
   [state]
   [:div#home-page
-   [:div.box
-    [:ul
-     [:li "Hi, this is my Clojurescript web app thing."]
-     [:li "The client is a statically hosted Github Page. " [:a.link {:href "https://github.com/akjetma/akjetma.github.io"} "[src]"]]
-     [:li "There's a little API running on Heroku for the wordcloud generator. " [:a.link {:href "https://github.com/akjetma/akjetma-back"} "[src]"]]]]])
+   [:div "Adam Jetmalani"]])
